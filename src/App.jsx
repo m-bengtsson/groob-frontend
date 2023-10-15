6d9f698ce9654587dd4c401853e371b0028dec00
Kate <kate.nordqvist@gmail.com>
2023-10-15 12:58:12 +0200

    move styled components to styles folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import { Link, Route, Routes } from "react-router-dom";
 import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
-import { StyledButton, LargeButton } from "../components/Button.styled";
-import { GlobalStyle } from "../components/Global.styled";
+import { StyledButton, LargeButton } from "./styles/Button.styled";
+import { GlobalStyle } from "./styles/Global.styled";
 
 function App() {
 	return (
 		<div>
 			<GlobalStyle />
-			<StyledButton>
-				<Link to="/signup">Signup</Link>
-			</StyledButton>
+
+			<Link to="/signup">
+				<StyledButton>Signup</StyledButton>
+			</Link>
+
 			<br />
-			<LargeButton>
-				<Link to="/login">Log in</Link>
-			</LargeButton>
+
+			<Link to="/login">
+				<LargeButton>Log in</LargeButton>
+			</Link>
 
 			<Routes>
 				<Route path="/login" element={<Login />} />
