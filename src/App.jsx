2c3d4517341e6b1ca2f30876cc3d7e76548e6e95
Kate <110845948+KateN87@users.noreply.github.com>
2023-10-15 12:59:28 +0200

    3 frontend large button (#4)
    * global styles
    
    remove padding and margin
    box-sizing: border-box
    removed styling on a
    a:visited, a: active: color inherit
    
    * styled component button & large button
    
    * replace Button with StyleButton
    
    * move styled components to styles folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import { Link, Route, Routes } from "react-router-dom";
 import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
+import { StyledButton, LargeButton } from "./styles/Button.styled";
+import { GlobalStyle } from "./styles/Global.styled";
 
 function App() {
 	return (
 		<div>
-			<Link to="/signup">Signup</Link>
+			<GlobalStyle />
+
+			<Link to="/signup">
+				<StyledButton>Signup</StyledButton>
+			</Link>
+
 			<br />
-			<Link to="/login">Log in</Link>
+
+			<Link to="/login">
+				<LargeButton>Log in</LargeButton>
+			</Link>
+
 			<Routes>
 				<Route path="/login" element={<Login />} />
 				<Route path="/signup" element={<Signup />} />
