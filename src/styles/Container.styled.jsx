0dc6275c7366f71f4dae5f804ad3afca0ae654e0
Kate <kate.nordqvist@gmail.com>
2023-10-15 17:35:03 +0200

    style StyledLoginSignupWrapper

---
+++ src/styles/Container.styled.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledLoginSignupWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 50%;
+	height: 40vh;
+	min-width: 220px;
+	align-items: center;
+	justify-content: space-around;
+`;
