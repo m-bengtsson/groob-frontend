1b3a71e95cc2e4058bbba8c3a3171eb69ee77a29
Kate <kate.nordqvist@gmail.com>
2023-10-18 15:23:06 +0200

    login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Link, Route, Routes } from "react-router-dom";
-import { Login } from "./pages/Login";
+import { LoginPage } from "./pages/LoginPage";
 import { SignupPage } from "./pages/SignupPage";
 import { StyledButton, LargeButton } from "./styles/Button.styled";
 import { GlobalStyle } from "./styles/Global.styled";
@@ -17,7 +17,7 @@ function App() {
 			</Link>
 
 			<Routes>
-				<Route path="/login" element={<Login />} />
+				<Route path="/login" element={<LoginPage />} />
 				<Route path="/signup" element={<SignupPage />} />
 			</Routes>
 		</div>
---
+++ src/components/Login.jsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+import { LargeButton } from "../styles/Button.styled";
+import { StyledLoginSignupWrapper } from "../styles/Container.styled";
+import { StyledInput, StyledInputLabel } from "../styles/Input.styled";
+
+export const Login = () => {
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const { email, password } = e.target;
+
+		console.log("EMAIL, PASSWORD", email.value, password.value);
+	};
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<StyledLoginSignupWrapper>
+					<StyledInputLabel>
+						<p>Email</p>
+						<StyledInput name="email" type="email" placeholder="Email" />
+					</StyledInputLabel>
+
+					<StyledInputLabel>
+						<p>Password</p>
+						<StyledInput
+							name="password"
+							type="password"
+							placeholder="Password"
+						/>
+					</StyledInputLabel>
+					<p>
+						Don`t have an account?{" "}
+						<Link to="/Signup">
+							<b>Sign up here.</b>
+						</Link>
+					</p>
+
+					<LargeButton type="submit">Log in</LargeButton>
+				</StyledLoginSignupWrapper>
+			</form>
+		</div>
+	);
+};
