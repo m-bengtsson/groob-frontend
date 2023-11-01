d2789158ab77800902aa124f4a6c00121df383b0
Kate <110845948+KateN87@users.noreply.github.com>
2023-11-01 16:08:56 +0100

    automatically login user (#22)
    make a get req to /currentUser with localstorage and/or cookie tokens

--- src/context/currentUser.jsx
+++ src/context/currentUser.jsx
@@ -1,14 +1,33 @@
 import { createContext, useEffect, useState } from "react";
+import axios from "axios";
 
 export const CurrentUserContext = createContext();
 
 export const CurrentUserProvider = ({ children }) => {
 	const [currentUser, setCurrentUser] = useState();
 
-	/* useEffect(() => {
-		const savedUser = JSON.parse(localStorage.getItem("accesstoken")) || [];
-		setCurrentUser(savedUser);
-	}, []); */
+	useEffect(() => {
+		const getUser = async () => {
+			const accesstoken = localStorage.getItem("accesstoken");
+
+			try {
+				const respUser = await axios({
+					method: "get",
+					url: "http://localhost:8080/api/users/currentUser",
+					withCredentials: true,
+					headers: { Authorization: accesstoken },
+				});
+
+				const user = await respUser.data;
+
+				setCurrentUser(user);
+			} catch (error) {
+				setCurrentUser(null);
+			}
+		};
+
+		getUser();
+	}, []);
 
 	const addCurrentUser = (user) => {
 		setCurrentUser(user);
