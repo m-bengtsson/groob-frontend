368a2ba4e3236e37e53a252dbf5da083c46216f6
Kate <kate.nordqvist@gmail.com>
2023-11-01 14:56:48 +0100

    add withCredentials in handleLogout request

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,26 +3,30 @@ import { CurrentUserContext } from "../context/currentUser";
 import axios from "axios";
 
 const HomePage = () => {
-  const { removeCurrentUser, currentUser } = useContext(CurrentUserContext);
+	const { removeCurrentUser, currentUser } = useContext(CurrentUserContext);
 
-  const handleLogout = async () => {
-    try {
-      await axios.post("http://localhost:8080/api/identity/logout", {
-        id: currentUser.id,
-      });
-    } catch (error) {
-      console.log(error);
-    }
+	const handleLogout = async () => {
+		try {
+			await axios.post(
+				"http://localhost:8080/api/identity/logout",
+				{
+					id: currentUser.id,
+				},
+				{ withCredentials: true }
+			);
 
-    removeCurrentUser();
-  };
+			removeCurrentUser();
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  return (
-    <div>
-      HomePage
-      <button onClick={handleLogout}>Logout</button>
-    </div>
-  );
+	return (
+		<div>
+			HomePage
+			<button onClick={handleLogout}>Logout</button>
+		</div>
+	);
 };
 
 export default HomePage;
