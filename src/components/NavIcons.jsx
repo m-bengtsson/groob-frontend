7979533984a05aea8ecc9838fcd9d8e955e47579
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-11-03 15:58:33 +0100

    Remove duplications for logout

--- src/components/NavIcons.jsx
+++ src/components/NavIcons.jsx
@@ -1,39 +1,10 @@
-import { Link } from "react-router-dom";
 import { View } from "../styles/Global.styled";
-import { useContext } from "react";
-import { CurrentUserContext } from "../context/currentUser";
-import axios from "axios";
 import { HiOutlineShoppingBag, HiOutlineHeart } from "react-icons/hi2";
 import { VscAccount } from "react-icons/vsc";
 
 const NavIcons = () => {
-  const { removeCurrentUser, currentUser } = useContext(CurrentUserContext);
-
-  const handleLogout = async () => {
-    try {
-      await axios.post(
-        "http://localhost:8080/api/identity/logout",
-        {
-          id: currentUser.id,
-        },
-        { withCredentials: true }
-      );
-
-      removeCurrentUser();
-    } catch (error) {
-      console.log(error);
-    }
-  };
   return (
     <View>
-      {currentUser ? (
-        <p onClick={handleLogout}>Logout</p>
-      ) : (
-        <Link to="/login">
-          <p>Login</p>
-        </Link>
-      )}
-
       <VscAccount size={22} />
       <HiOutlineHeart size={22} />
       <HiOutlineShoppingBag size={22} />
