a039894455e4b77446d5a641c1ee2c315315dfb9
Kate <kate.nordqvist@gmail.com>
2023-11-04 11:36:55 +0100

    add button and fixed custom table name

--- src/pages/ManageUsersPage.jsx
+++ src/pages/ManageUsersPage.jsx
@@ -1,5 +1,6 @@
-import UsersTable from "../components/UsersTable";
+import CustomTable from "../components/CustomTable";
 import mockUsers from "../MockData/mockUsers.json";
+import { SmallButton } from "../styles/Button.styled";
 import { StyledManageUsers } from "../styles/Container.styled";
 
 const ManageUsersPage = () => {
@@ -15,18 +16,25 @@ const ManageUsersPage = () => {
 		"Updated",
 	];
 
+	const addUserHandler = () => {
+		console.log("ADD USER!");
+	};
 	return (
 		<StyledManageUsers>
 			<h2>Manage users</h2>
 
 			<div>
-				<h5>Admin Users</h5>
-				<UsersTable users={roleAdmin} titles={titles} />
+				<div className={"title-btn-container"}>
+					<h5>Admin Users</h5>
+					<SmallButton onClick={addUserHandler}>+ Add User</SmallButton>
+				</div>
+
+				<CustomTable data={roleAdmin} titles={titles} />
 			</div>
 
 			<div>
 				<h5>User Users</h5>
-				<UsersTable users={roleUsers} titles={titles} />
+				<CustomTable data={roleUsers} titles={titles} />
 			</div>
 		</StyledManageUsers>
 	);
