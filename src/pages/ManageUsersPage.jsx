1bcced9f6e9a19c5b432e0ca5b7bb695372bb61d
Kate <kate.nordqvist@gmail.com>
2023-11-05 12:15:56 +0100

    get users from server

--- src/pages/ManageUsersPage.jsx
+++ src/pages/ManageUsersPage.jsx
@@ -1,11 +1,13 @@
+import { useEffect, useState } from "react";
 import CustomTable from "../components/CustomTable";
-import mockUsers from "../MockData/mockUsers.json";
 import { SmallButton } from "../styles/Button.styled";
 import { StyledManageUsers } from "../styles/Container.styled";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const ManageUsersPage = () => {
-	const roleUsers = mockUsers.filter((user) => user.role === "user");
-	const roleAdmin = mockUsers.filter((user) => user.role === "admin");
+	const navigate = useNavigate();
+	const [users, setUsers] = useState();
 	const titles = [
 		"id",
 		"Name",
@@ -16,9 +18,57 @@ const ManageUsersPage = () => {
 		"Updated",
 	];
 
+	useEffect(() => {
+		const getUsers = async () => {
+			const accesstoken = localStorage.getItem("accesstoken");
+
+			try {
+				const respAccess = await axios({
+					method: "get",
+					url: "http://localhost:8080/api/users/",
+					withCredentials: false,
+					headers: { Authorization: accesstoken },
+				});
+
+				const allUsers = await respAccess.data;
+				setUsers(allUsers);
+			} catch (error) {
+				if (error.code === "ERR_BAD_REQUEST") {
+					try {
+						const respRefresh = await axios({
+							method: "get",
+							url: "http://localhost:8080/api/users",
+							withCredentials: true,
+						});
+
+						const allUsers = await respRefresh.data;
+						const accesstoken = respRefresh.headers.authorization;
+
+						setUsers(allUsers);
+						localStorage.setItem("accesstoken", accesstoken);
+						return;
+					} catch (error) {
+						console.log("Not authorized, please login1");
+					}
+				}
+
+				console.log("Not authorized, please login2");
+			}
+		};
+		getUsers();
+	}, []);
+
+	const roleUser = users?.filter((user) => user.role === "user");
+	const roleAdmin = users?.filter((user) => user.role === "admin");
+
 	const addUserHandler = () => {
 		console.log("ADD USER!");
 	};
+
+	if (!users) {
+		return <div>Loading...</div>;
+	}
+
 	return (
 		<StyledManageUsers>
 			<h2>Manage users</h2>
@@ -34,7 +84,7 @@ const ManageUsersPage = () => {
 
 			<div>
 				<h5>User Users</h5>
-				<CustomTable data={roleUsers} titles={titles} />
+				<CustomTable data={roleUser} titles={titles} />
 			</div>
 		</StyledManageUsers>
 	);
