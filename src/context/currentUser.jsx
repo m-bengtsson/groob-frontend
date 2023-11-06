b9e6fa899c3cc758ef3127baa04aa06d895429f9
Kate <110845948+KateN87@users.noreply.github.com>
2023-11-06 11:23:30 +0100

    Add axios interceptor (#37)
    - request interceptor which adds accesstoken
    - response interceptor which adds refreshtoken if error status 401 and data is "No accesstoken provided"

--- src/context/currentUser.jsx
+++ src/context/currentUser.jsx
@@ -1,69 +1,46 @@
 import { createContext, useEffect, useState } from "react";
 import axios from "axios";
+import instance from "../axiosconfig";
 
 export const CurrentUserContext = createContext();
 
 export const CurrentUserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState();
+	const [currentUser, setCurrentUser] = useState();
 
-  useEffect(() => {
-    const getUser = async () => {
-      const accesstoken = localStorage.getItem("accesstoken");
+	useEffect(() => {
+		const getUser = async () => {
+			try {
+				const respAccess = await instance.get("/users/currentUser");
 
-      try {
-        const respAccess = await axios({
-          method: "get",
-          url: "http://localhost:8080/api/users/currentUser",
-          withCredentials: false,
-          headers: { Authorization: accesstoken },
-        });
+				const user = await respAccess.data;
+				setCurrentUser(user);
+			} catch (error) {
+				//todo: handle this better
+				console.log("You are not logged in");
+			}
+		};
+		getUser();
+	}, []);
 
-        const user = await respAccess.data;
-        setCurrentUser(user);
-      } catch (error) {
-        if (error.code === "ERR_BAD_REQUEST") {
-          try {
-            const respRefresh = await axios({
-              method: "get",
-              url: "http://localhost:8080/api/users/currentUser",
-              withCredentials: true,
-            });
+	const addCurrentUser = (user) => {
+		setCurrentUser(user);
+	};
 
-            const user = await respRefresh.data;
-            const accesstoken = respRefresh.headers.authorization;
+	const removeCurrentUser = () => {
+		setCurrentUser(null);
+		localStorage.removeItem("accesstoken");
+	};
 
-            setCurrentUser(user);
-            localStorage.setItem("accesstoken", accesstoken);
-            return;
-          } catch (error) {
-            setCurrentUser(null);
-          }
-        }
-        setCurrentUser(null);
-      }
-    };
-    getUser();
-  }, []);
+	const value = {
+		currentUser,
+		addCurrentUser,
+		removeCurrentUser,
+	};
 
-  const addCurrentUser = (user) => {
-    setCurrentUser(user);
-  };
-
-  const removeCurrentUser = () => {
-    setCurrentUser(null);
-    localStorage.removeItem("accesstoken");
-  };
-
-  const value = {
-    currentUser,
-    addCurrentUser,
-    removeCurrentUser,
-  };
-
-  return (
-    <CurrentUserContext.Provider value={value}>
-      {children}
-    </CurrentUserContext.Provider>
-  );
+	return (
+		<CurrentUserContext.Provider value={value}>
+			{children}
+		</CurrentUserContext.Provider>
+	);
 };
 export default CurrentUserProvider;
