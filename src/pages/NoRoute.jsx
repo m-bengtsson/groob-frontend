d201bcd1db563b40206939294952ea1a1ffa5b00
Kate <kate.nordqvist@gmail.com>
2023-11-06 21:12:40 +0100

    remove console.log

--- src/pages/NoRoute.jsx
+++ src/pages/NoRoute.jsx
@@ -2,7 +2,7 @@ import { useLocation } from "react-router-dom";
 
 const NoRoute = () => {
 	const location = useLocation();
-	console.log("LOC: ", location.pathname);
+
 	return (
 		<>
 			<p>Oh no, we couldnt find a route for {location.pathname}</p>
