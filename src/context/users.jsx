1b2692f49e26e6b0efe83e7695328a2e267b29f8
Kate <kate.nordqvist@gmail.com>
2023-11-07 19:18:19 +0100

    add errormessage in users context

--- src/context/users.jsx
+++ src/context/users.jsx
@@ -3,11 +3,14 @@ import { createContext, useState } from "react";
 export const UsersContext = createContext();
 
 export const UsersContextProvider = ({ children }) => {
-	const [users, setUsers] = useState();
+	const [users, setUsers] = useState([]);
+	const [errorMessage, setErrorMessage] = useState("");
 
 	const value = {
 		users,
 		setUsers,
+		errorMessage,
+		setErrorMessage,
 	};
 
 	return (
