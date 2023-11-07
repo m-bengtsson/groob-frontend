4a5c296e8b34d504633af066bcf3f51dd2176531
Kate <kate.nordqvist@gmail.com>
2023-11-07 14:59:14 +0100

    add UsersContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,17 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import CurrentUserProvider from "./context/currentUser.jsx";
 import ItemsContextProvider from "./context/items.jsx";
+import UsersContextProvider from "./context/users.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<CurrentUserProvider>
-				<ItemsContextProvider>
-					<App />
-				</ItemsContextProvider>
+				<UsersContextProvider>
+					<ItemsContextProvider>
+						<App />
+					</ItemsContextProvider>
+				</UsersContextProvider>
 			</CurrentUserProvider>
 		</BrowserRouter>
 	</React.StrictMode>
