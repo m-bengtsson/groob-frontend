6214fabfa157aced15f2613396e405d2d1c837e2
Kate <kate.nordqvist@gmail.com>
2023-11-07 20:13:21 +0100

    add functionality to manageItemsPage

--- src/pages/ManageItemsPage.jsx
+++ src/pages/ManageItemsPage.jsx
@@ -1,8 +1,101 @@
+import { useContext, useState, useEffect } from "react";
+import CustomTable from "../components/CustomTable";
+import { StyledManagePage } from "../styles/Container.styled";
+import { ItemsContext } from "../context/items";
+import instance from "../axiosconfig";
+import DeleteModal from "../components/DeleteModal";
+import EditItemModal from "../components/EditItemModal";
+
 const ManageItemsPage = () => {
+	const { items, setItems, errorMessage, setErrorMessage } =
+		useContext(ItemsContext);
+	const [showDeleteModal, setShowDeleteModal] = useState(false);
+	const [selectedItem, setSelectedItem] = useState();
+	const [showEditModal, setShowEditModal] = useState(false);
+
+	const titles = [
+		"id",
+		"Title",
+		"Description",
+		"Added By",
+		"Updated By",
+		"Created",
+		"Updated",
+	];
+
+	useEffect(() => {
+		setErrorMessage("");
+		if (items.length === 0) {
+			const getItems = async () => {
+				try {
+					const response = await instance.get("/items");
+					const items = response.data;
+					setItems(items);
+				} catch (error) {
+					if (error.response.status === 404) {
+						setErrorMessage("Oh no! We could not fetch the items :'(");
+					}
+				}
+			};
+			getItems();
+		}
+	}, [items, setErrorMessage, setItems]);
+
+	const editItem = (item) => {
+		if (item.name) {
+			setSelectedItem({ title: item.name, id: item.id, role: item.role });
+			setShowEditModal(true);
+		} else if (item.title) {
+			setSelectedItem({ title: item.title, id: item.id });
+			setShowEditModal(true);
+		}
+	};
+
+	const deleteItem = (item) => {
+		if (item.name) {
+			setSelectedItem({ title: item.name, id: item.id });
+			setShowDeleteModal(true);
+		} else if (item.title) {
+			setSelectedItem({ title: item.title, id: item.id });
+			setShowDeleteModal(true);
+		}
+	};
+
+	if (!items) {
+		return <div>Loading...</div>;
+	}
+
 	return (
-		<>
-			<p>Manage Items</p>
-		</>
+		<StyledManagePage>
+			<h2>Manage Items</h2>
+
+			<div>
+				<div className={"title-btn-container"}>
+					<h5>Manage Items</h5>
+				</div>
+				{errorMessage && <p>{errorMessage}</p>}
+				<CustomTable
+					data={items}
+					titles={titles}
+					editHandler={editItem}
+					deleteHandler={deleteItem}
+				/>
+			</div>
+
+			{showDeleteModal && (
+				<DeleteModal
+					setShowDeleteModal={setShowDeleteModal}
+					selectedItem={selectedItem}
+					dataType={"items"}
+				/>
+			)}
+			{showEditModal && (
+				<EditItemModal
+					setShowEditModal={setShowEditModal}
+					selectedItem={selectedItem}
+				/>
+			)}
+		</StyledManagePage>
 	);
 };
 
