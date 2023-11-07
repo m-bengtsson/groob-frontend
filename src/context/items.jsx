4a95c5cb09aa9cac18bfe65c1d04394ccf99b0d2
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-11-07 12:04:57 +0100

    Items context
    Context and rendering items

---
+++ src/context/items.jsx
@@ -0,0 +1,34 @@
+import { createContext, useEffect, useState } from "react";
+import instance from "../axiosconfig";
+
+export const ItemsContext = createContext();
+
+export const ItemsContextProvider = ({ children }) => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const getItems = async () => {
+      try {
+        const response = await instance.get("/items");
+        const items = await response.data;
+        setItems(items);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getItems();
+  }, []);
+
+  /*  const addItem = (item) => {
+    setItems(prev);
+  }; */
+
+  const value = {
+    items,
+  };
+
+  return (
+    <ItemsContext.Provider value={value}>{children}</ItemsContext.Provider>
+  );
+};
+export default ItemsContextProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import CurrentUserProvider from "./context/currentUser.jsx";
+import ItemsContextProvider from "./context/items.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<CurrentUserProvider>
-				<App />
-			</CurrentUserProvider>
-		</BrowserRouter>
-	</React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <CurrentUserProvider>
+        <ItemsContextProvider>
+          <App />
+        </ItemsContextProvider>
+      </CurrentUserProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
