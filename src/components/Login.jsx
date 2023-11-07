7ae2c0a987fec619156a4359ca8d8d56e0c1b2c5
Kate <kate.nordqvist@gmail.com>
2023-11-07 21:04:17 +0100

    change between login and password request

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,15 @@
-import { Link } from "react-router-dom";
 import { LargeButton } from "../styles/Button.styled";
 import { StyledLoginSignupWrapper } from "../styles/Container.styled";
 import { StyledInput, StyledInputLabel } from "../styles/Input.styled";
 import axios from "axios";
 import { CurrentUserContext } from "../context/currentUser";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 export const Login = () => {
 	const { addCurrentUser } = useContext(CurrentUserContext);
+	const [showRequestPassword, setShowRequestPassword] = useState(false);
 
-	const handleSubmit = async (e) => {
+	const handleLogin = async (e) => {
 		e.preventDefault();
 		const { email, password } = e.target;
 
@@ -42,28 +42,49 @@ export const Login = () => {
 		}
 	};
 
+	const handlePassword = (e) => {
+		e.preventDefault();
+		const { email } = e.target;
+		console.log("REQUEST PASSWORD EMAIL: ", email.value);
+	};
+
 	return (
 		<StyledLoginSignupWrapper>
-			<h1>Login</h1>
-			<form onSubmit={handleSubmit}>
-				<StyledInputLabel>
-					<p>Email</p>
-					<StyledInput name="email" type="email" placeholder="Email" />
-				</StyledInputLabel>
+			<h1>{showRequestPassword ? "Request Password" : "Login"}</h1>
+			{!showRequestPassword && (
+				<form onSubmit={handleLogin}>
+					<StyledInputLabel>
+						<p>Email</p>
+						<StyledInput name="email" type="email" placeholder="Email" />
+					</StyledInputLabel>
 
-				<StyledInputLabel>
-					<p>Password</p>
-					<StyledInput name="password" type="password" placeholder="Password" />
-				</StyledInputLabel>
-				<p>
-					Don`t have an account?
-					<Link to="/Signup">
-						<b>Sign up here.</b>
-					</Link>
-				</p>
+					<StyledInputLabel>
+						<p>Password</p>
+						<StyledInput
+							name="password"
+							type="password"
+							placeholder="Password"
+						/>
+					</StyledInputLabel>
+					<p onClick={() => setShowRequestPassword(true)}>
+						<b>Forgot Password?</b>
+					</p>
 
-				<LargeButton type="submit">Log in</LargeButton>
-			</form>
+					<LargeButton type="submit">Log in</LargeButton>
+				</form>
+			)}
+			{showRequestPassword && (
+				<form onSubmit={handlePassword}>
+					<StyledInputLabel>
+						<p>Email</p>
+						<StyledInput name="email" type="email" placeholder="Email" />
+					</StyledInputLabel>
+					<p onClick={() => setShowRequestPassword(false)}>
+						<b>Cancel request</b>
+					</p>
+					<LargeButton type="submit">Send Request</LargeButton>
+				</form>
+			)}
 		</StyledLoginSignupWrapper>
 	);
 };
