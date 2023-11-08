99535339d10f022fee2041b72663239f6198726e
Kate <kate.nordqvist@gmail.com>
2023-11-08 10:38:37 +0100

    Styled not found page

--- src/pages/NoRoute.jsx
+++ src/pages/NoRoute.jsx
@@ -1,12 +1,32 @@
-import { useLocation } from "react-router-dom";
+/* import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom"; */
+import { StyledNotFound } from "../styles/NotFound.styled";
+import notFoundImg from "../assets/not-found.jpg";
+import { LargeButton } from "../styles/Button.styled";
+import { Link } from "react-router-dom";
 
 const NoRoute = () => {
-	const location = useLocation();
+	/* const location = useLocation();
+	const navigate = useNavigate(); */
+
+	/* 	useEffect(() => {
+		const ReRoute = () => {
+			return setTimeout(() => navigate("/"), 3000);
+		};
+		ReRoute();
+	}, [navigate]); */
 
 	return (
-		<>
-			<p>Oh no, we couldnt find a route for {location.pathname}</p>
-		</>
+		<StyledNotFound>
+			<div>
+				<h1>OOPS, 404!</h1>
+				<h3>We can't seem to find this page</h3>
+				<Link to="/">
+					<LargeButton>Return to Home</LargeButton>
+				</Link>
+			</div>
+			<img src={notFoundImg} />
+		</StyledNotFound>
 	);
 };
 
---
+++ src/styles/NotFound.styled.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const StyledNotFound = styled.div`
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-content: space-evenly;
+
+	div {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-evenly;
+		height: 30vh;
+	}
+	h1 {
+		font-size: 42px;
+	}
+
+	img {
+		width: 20vw;
+		height: 40vh;
+	}
+`;
