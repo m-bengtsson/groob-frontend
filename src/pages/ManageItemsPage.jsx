58f5259db806a140a5b2c51c8300821331fcab40
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-11-08 23:52:44 +0100

    Navigate to addItemPage

--- src/pages/ManageItemsPage.jsx
+++ src/pages/ManageItemsPage.jsx
@@ -5,99 +5,108 @@ import { ItemsContext } from "../context/items";
 import instance from "../axiosconfig";
 import DeleteModal from "../components/DeleteModal";
 import EditItemModal from "../components/EditItemModal";
+import { SmallButton } from "../styles/Button.styled";
+import { useNavigate } from "react-router-dom";
 
 const ManageItemsPage = () => {
-	const { items, setItems, errorMessage, setErrorMessage } =
-		useContext(ItemsContext);
+  const { items, setItems, errorMessage, setErrorMessage } =
+    useContext(ItemsContext);
 
-	const [showDeleteModal, setShowDeleteModal] = useState(false);
-	const [selectedItem, setSelectedItem] = useState();
-	const [showEditModal, setShowEditModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [selectedItem, setSelectedItem] = useState();
+  const [showEditModal, setShowEditModal] = useState(false);
 
-	const titles = [
-		"id",
-		"Title",
-		"Description",
-		"In stock",
-		"Added By",
-		"Updated By",
-		"Created",
-		"Updated",
-	];
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		setErrorMessage("");
-		if (items.length === 0) {
-			const getItems = async () => {
-				try {
-					const response = await instance.get("/items");
-					const items = response.data;
-					setItems(items);
-				} catch (error) {
-					if (error.response.status === 404) {
-						setErrorMessage("Oh no! We could not fetch the items :'(");
-					}
-				}
-			};
-			getItems();
-		}
-	}, [items, setErrorMessage, setItems]);
+  const titles = [
+    "id",
+    "Title",
+    "Description",
+    "In stock",
+    "Added By",
+    "Updated By",
+    "Created",
+    "Updated",
+  ];
 
-	const editItem = (item) => {
-		if (item.name) {
-			setSelectedItem({ title: item.name, id: item.id, role: item.role });
-			setShowEditModal(true);
-		} else if (item.title) {
-			setSelectedItem({ title: item.title, id: item.id });
-			setShowEditModal(true);
-		}
-	};
+  useEffect(() => {
+    setErrorMessage("");
+    if (items.length === 0) {
+      const getItems = async () => {
+        try {
+          const response = await instance.get("/items");
+          const items = response.data;
+          setItems(items);
+        } catch (error) {
+          if (error.response.status === 404) {
+            setErrorMessage("Oh no! We could not fetch the items :'(");
+          }
+        }
+      };
+      getItems();
+    }
+  }, [items, setErrorMessage, setItems]);
 
-	const deleteItem = (item) => {
-		if (item.name) {
-			setSelectedItem({ title: item.name, id: item.id });
-			setShowDeleteModal(true);
-		} else if (item.title) {
-			setSelectedItem({ title: item.title, id: item.id });
-			setShowDeleteModal(true);
-		}
-	};
+  const addItem = () => {};
 
-	if (!items) {
-		return <div>Loading...</div>;
-	}
+  const editItem = (item) => {
+    if (item.name) {
+      setSelectedItem({ title: item.name, id: item.id, role: item.role });
+      setShowEditModal(true);
+    } else if (item.title) {
+      setSelectedItem({ title: item.title, id: item.id });
+      setShowEditModal(true);
+    }
+  };
 
-	return (
-		<StyledManagePage>
-			<h2>Manage Items</h2>
+  const deleteItem = (item) => {
+    if (item.name) {
+      setSelectedItem({ title: item.name, id: item.id });
+      setShowDeleteModal(true);
+    } else if (item.title) {
+      setSelectedItem({ title: item.title, id: item.id });
+      setShowDeleteModal(true);
+    }
+  };
 
-			<div>
-				<div className={"title-btn-container"}>
-					<h5>Manage Items</h5>
-				</div>
-				{errorMessage && <p>{errorMessage}</p>}
-				<CustomTable
-					data={items}
-					titles={titles}
-					editHandler={editItem}
-					deleteHandler={deleteItem}
-				/>
-			</div>
-			{showDeleteModal && (
-				<DeleteModal
-					setShowDeleteModal={setShowDeleteModal}
-					selectedItem={selectedItem}
-					dataType={"items"}
-				/>
-			)}
-			{showEditModal && (
-				<EditItemModal
-					setShowEditModal={setShowEditModal}
-					selectedItem={selectedItem}
-				/>
-			)}
-		</StyledManagePage>
-	);
+  if (!items) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <StyledManagePage>
+      <h2>Manage Items</h2>
+
+      <div>
+        <div className={"title-btn-container"}>
+          <h5>Manage Items</h5>
+        </div>
+        {errorMessage && <p>{errorMessage}</p>}
+        <SmallButton onClick={() => navigate("add-item")}>
+          + Add item
+        </SmallButton>
+        <CustomTable
+          data={items}
+          titles={titles}
+          editHandler={editItem}
+          deleteHandler={deleteItem}
+        />
+      </div>
+      {showDeleteModal && (
+        <DeleteModal
+          setShowDeleteModal={setShowDeleteModal}
+          selectedItem={selectedItem}
+          dataType={"items"}
+        />
+      )}
+      {showEditModal && (
+        <EditItemModal
+          setShowEditModal={setShowEditModal}
+          selectedItem={selectedItem}
+        />
+      )}
+    </StyledManagePage>
+  );
 };
 
 export default ManageItemsPage;
