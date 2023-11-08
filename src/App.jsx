7ed2d54184e0be476caafe33ff8e637fa3f8885d
Matilda <113067882+m-bengtsson@users.noreply.github.com>
2023-11-08 21:28:29 +0100

    55 frontend signup (#60)
    * Signup route
    
    * Sign up post request
    
    * Set error message

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { useContext } from "react";
 import { LoginPage } from "./pages/LoginPage";
 import { GlobalStyle } from "./styles/Global.styled";
+import { SignupPage } from "./pages/SignupPage";
 import HomePage from "./pages/HomePage";
 import { CurrentUserContext } from "./context/currentUser";
 import AdminPage from "./pages/AdminPage";
@@ -14,41 +15,42 @@ import NoRoute from "./pages/NoRoute";
 import Reset from "./pages/Reset";
 
 function App() {
-	const { currentUser, isLoading } = useContext(CurrentUserContext);
+  const { currentUser, isLoading } = useContext(CurrentUserContext);
 
-	if (isLoading) {
-		return <div>Loading...</div>;
-	}
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-	return (
-		<div>
-			<GlobalStyle />
-			<Routes>
-				{currentUser && currentUser.role === "admin" && (
-					<Route path="/" element={<AdminTemplate />}>
-						<Route path="/" element={<AdminPage />} />
-						<Route path="manage-users" element={<ManageUsersPage />} />
-						<Route path="manage-items" element={<ManageItemsPage />} />
-					</Route>
-				)}
+  return (
+    <div>
+      <GlobalStyle />
+      <Routes>
+        {currentUser && currentUser.role === "admin" && (
+          <Route path="/" element={<AdminTemplate />}>
+            <Route path="/" element={<AdminPage />} />
+            <Route path="manage-users" element={<ManageUsersPage />} />
+            <Route path="manage-items" element={<ManageItemsPage />} />
+          </Route>
+        )}
 
-				{currentUser && currentUser.role === "user" && (
-					<Route path="/" element={<UserTemplate />}>
-						<Route path="/" element={<HomePage />} />
-					</Route>
-				)}
+        {currentUser && currentUser.role === "user" && (
+          <Route path="/" element={<UserTemplate />}>
+            <Route path="/" element={<HomePage />} />
+          </Route>
+        )}
 
-				{!currentUser && (
-					<Route path="/" element={<PublicTemplate />}>
-						<Route path="/" element={<LoginPage />} />
-						<Route path="reset" element={<Reset />} />
-					</Route>
-				)}
+        {!currentUser && (
+          <Route path="/" element={<PublicTemplate />}>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="reset" element={<Reset />} />
+            <Route path="signup" element={<SignupPage />} />
+          </Route>
+        )}
 
-				<Route path="*" element={<NoRoute />} />
-			</Routes>
-		</div>
-	);
+        <Route path="*" element={<NoRoute />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,52 +1,75 @@
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import { LargeButton } from "../styles/Button.styled";
 import { StyledLoginSignupWrapper } from "../styles/Container.styled";
 import { StyledInput, StyledInputLabel } from "../styles/Input.styled";
 
 export const Signup = () => {
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		const { email, password, rePassword } = e.target;
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
 
-		console.log(
-			"EMAIL, PASSWORD, REPASSWORD",
-			email.value,
-			password.value,
-			rePassword.value
-		);
-	};
+  const [searchParams] = useSearchParams();
+  const signupToken = searchParams.get("token_key");
 
-	return (
-		<StyledLoginSignupWrapper>
-			<h1>Sign up</h1>
-			<form onSubmit={handleSubmit}>
-				<StyledInputLabel>
-					<p>Email</p>
-					<StyledInput name="email" type="email" placeholder="Email" />
-				</StyledInputLabel>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { name, password, rePassword } = e.target;
 
-				<StyledInputLabel>
-					<p>Password</p>
-					<StyledInput name="password" type="password" placeholder="Password" />
-				</StyledInputLabel>
+    const body = {
+      name: name.value,
+      password: password.value,
+      repeatPassword: rePassword.value,
+    };
 
-				<StyledInputLabel>
-					<p>Repeat Password</p>
-					<StyledInput
-						name="rePassword"
-						type="password"
-						placeholder="Password"
-					/>
-				</StyledInputLabel>
-				<p>
-					Already have an account?{" "}
-					<Link to="/">
-						<b>Log in here.</b>
-					</Link>
-				</p>
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/identity/register",
+        body,
+        {
+          headers: { Authorization: signupToken },
+        }
+      );
+      if (response.status === 200) {
+        navigate("/");
+      }
+    } catch (error) {
+      setErrorMessage("Oops! something went wrong with the signup");
+    }
+  };
 
-				<LargeButton type="submit">Sign up</LargeButton>
-			</form>
-		</StyledLoginSignupWrapper>
-	);
+  return (
+    <StyledLoginSignupWrapper>
+      <h1>Sign up</h1>
+      {errorMessage && <p>{errorMessage}</p>}
+      <form onSubmit={handleSubmit}>
+        <StyledInputLabel>
+          <p>Name</p>
+          <StyledInput name="name" type="text" placeholder="Name" />
+        </StyledInputLabel>
+
+        <StyledInputLabel>
+          <p>Password</p>
+          <StyledInput name="password" type="password" placeholder="Password" />
+        </StyledInputLabel>
+
+        <StyledInputLabel>
+          <p>Repeat Password</p>
+          <StyledInput
+            name="rePassword"
+            type="password"
+            placeholder="Password"
+          />
+        </StyledInputLabel>
+        <p>
+          Already have an account?{" "}
+          <Link to="/">
+            <b>Log in here.</b>
+          </Link>
+        </p>
+
+        <LargeButton type="submit">Sign up</LargeButton>
+      </form>
+    </StyledLoginSignupWrapper>
+  );
 };
