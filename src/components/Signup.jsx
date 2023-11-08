0655a3e355343a40f03f8e0b22fa9c0b17350aee
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-11-08 21:27:53 +0100

    Set error message

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,13 +1,15 @@
-import { Link } from "react-router-dom";
 import axios from "axios";
-import { useSearchParams } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import { LargeButton } from "../styles/Button.styled";
 import { StyledLoginSignupWrapper } from "../styles/Container.styled";
 import { StyledInput, StyledInputLabel } from "../styles/Input.styled";
 
 export const Signup = () => {
-  const [searchParams] = useSearchParams();
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
 
+  const [searchParams] = useSearchParams();
   const signupToken = searchParams.get("token_key");
 
   const handleSubmit = async (e) => {
@@ -28,22 +30,18 @@ export const Signup = () => {
           headers: { Authorization: signupToken },
         }
       );
-      console.log("RESPONSE REGISTER: ", response);
+      if (response.status === 200) {
+        navigate("/");
+      }
     } catch (error) {
-      console.log("ERROR REGISTER USER: ", error);
+      setErrorMessage("Oops! something went wrong with the signup");
     }
-
-    /*  console.log(
-      "EMAIL, PASSWORD, REPASSWORD",
-      email.value,
-      password.value,
-      rePassword.value
-    ); */
   };
 
   return (
     <StyledLoginSignupWrapper>
       <h1>Sign up</h1>
+      {errorMessage && <p>{errorMessage}</p>}
       <form onSubmit={handleSubmit}>
         <StyledInputLabel>
           <p>Name</p>
