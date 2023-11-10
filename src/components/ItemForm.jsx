a59a7f0b4266955abd41283d2372f13d6fcb5234
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-11-10 11:32:22 +0100

    Handle onchange on item details
    onChange on inputs to preview item details

--- src/components/ItemForm.jsx
+++ src/components/ItemForm.jsx
@@ -2,16 +2,26 @@ import { StyledInput } from "../styles/Input.styled";
 import { StyledInputLabel } from "../styles/Input.styled";
 import { StyledButton } from "../styles/Button.styled";
 
-const ItemForm = ({ handleSubmit }) => {
+const ItemForm = ({ handleSubmit, handleInputChange }) => {
   return (
     <form onSubmit={handleSubmit}>
       <StyledInputLabel>
         <p>Title</p>
-        <StyledInput name="title" type="text" placeholder="Title" />
+        <StyledInput
+          name="title"
+          type="text"
+          placeholder="Title"
+          onChange={handleInputChange}
+        />
       </StyledInputLabel>
       <StyledInputLabel>
         <p>Description</p>
-        <StyledInput name="description" type="text" placeholder="description" />
+        <StyledInput
+          name="description"
+          type="text"
+          placeholder="description"
+          onChange={handleInputChange}
+        />
       </StyledInputLabel>
       <StyledInputLabel>
         <p>Items in stock</p>
@@ -19,10 +29,10 @@ const ItemForm = ({ handleSubmit }) => {
           name="itemsInStock"
           type="number"
           placeholder="Number of items"
+          onChange={handleInputChange}
         />
       </StyledInputLabel>
       <StyledButton>Upload Image</StyledButton>
-      <StyledButton>Item preview</StyledButton>
       <button type="submit">Add item</button>
     </form>
   );
--- src/pages/AddItemPage.jsx
+++ src/pages/AddItemPage.jsx
@@ -1,8 +1,15 @@
 import { StyledAddItemPage } from "../styles/Container.styled";
 import instance from "../axiosconfig";
 import ItemForm from "../components/ItemForm";
+import { useState } from "react";
 
 const AddItemPage = () => {
+  const [formData, setFormData] = useState({
+    title: null,
+    description: null,
+    itemsInStock: null,
+  });
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { title, description, itemsInStock } = e.target;
@@ -15,17 +22,34 @@ const AddItemPage = () => {
 
     try {
       const response = await instance.post("/items", body);
-      console.log("response: ", response);
+      // todo modal or text for successful
     } catch (error) {
       console.log(error);
     }
   };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   return (
     <>
       <h2>Add item</h2>
       <StyledAddItemPage>
-        <ItemForm handleSubmit={handleSubmit} />
-        <div>Item Overview</div>
+        <ItemForm
+          handleSubmit={handleSubmit}
+          handleInputChange={handleInputChange}
+        />
+        <div>
+          Item Overview
+          <p>{formData.title}</p>
+          <p>{formData.description}</p>
+          <p>{formData.itemsInStock}</p>
+        </div>
       </StyledAddItemPage>
     </>
   );
