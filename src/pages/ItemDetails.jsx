a91179650c769d830f5b55d5fd218a569dfe6471
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-11-12 19:08:13 +0100

    Display item properties set errormessage

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,23 +1,41 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import instance from "../axiosconfig";
+import { SmallButton } from "../styles/Button.styled";
 
 const ItemDetails = () => {
   const { id } = useParams();
+  const [fethcedItem, setItem] = useState();
+  const [errorMessage, setErrorMessage] = useState("");
+  // todo: loader
 
   useEffect(() => {
-    const getItems = async () => {
+    const getItem = async () => {
       try {
         const response = await instance.get(`items/${id}`);
-        const item = response.data;
+        const item = await response.data;
+        setItem(item);
         console.log("ITEM: ", item);
       } catch (error) {
-        console.log("ERROR: ", error);
+        setErrorMessage("We could not find what you are looking for");
       }
     };
-    getItems();
+    getItem();
   }, [id]);
-  return <div>Item details</div>;
+
+  return (
+    <div>
+      <div>
+        <div>Image</div>
+      </div>
+      <div>
+        <h3>{fethcedItem?.title}</h3>
+        <p>{fethcedItem?.description}</p>
+        <SmallButton>Remind me</SmallButton>
+        <SmallButton>Buy</SmallButton>
+      </div>
+    </div>
+  );
 };
 
 export default ItemDetails;
