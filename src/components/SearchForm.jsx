d5f29cb07b66d97ce665d0164f77f1b910dda026
Kate <kate.nordqvist@gmail.com>
2023-11-13 15:25:00 +0100

    create searchForm.jsx

---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,52 @@
+import { useContext, useState } from "react";
+import { ItemsContext } from "../context/items";
+import axios from "axios";
+import { SmallStyledInput, StyledInputLabel } from "../styles/Input.styled";
+import ErrorMessage from "./ErrorMessage";
+
+const SearchForm = () => {
+	const { setItems } = useContext(ItemsContext);
+	const [errorMessage, setErrorMessage] = useState("");
+
+	const searchItem = async (e) => {
+		e.preventDefault();
+		setErrorMessage("");
+
+		let searchedItem = e.target.item.value;
+		searchedItem = searchedItem.split(" ").join("_");
+
+		try {
+			const response = await axios.get(
+				`http://localhost:8080/api/items?title=${searchedItem}`
+			);
+
+			e.target.item.value = "";
+
+			if (response.data.length === 0) {
+				e.target.item.value = "";
+				return setErrorMessage(
+					`We could not find any items matching "${searchedItem}"`
+				);
+			}
+
+			setItems(response.data);
+		} catch (error) {
+			return setErrorMessage("Something went wrong, please try again later");
+		}
+	};
+
+	return (
+		<form onSubmit={searchItem}>
+			<StyledInputLabel>
+				<SmallStyledInput
+					type="text"
+					name="item"
+					placeholder="Search item..."
+				/>
+			</StyledInputLabel>
+			{errorMessage && <ErrorMessage message={errorMessage} />}
+		</form>
+	);
+};
+
+export default SearchForm;
