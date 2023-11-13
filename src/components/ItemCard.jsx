074f170db7d965a8d8e7b0fbc251752f789d99c1
Kate <kate.nordqvist@gmail.com>
2023-11-13 09:43:33 +0100

    link logo to homepage

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,19 +1,17 @@
 import { Link } from "react-router-dom";
 import defaultImage from "../assets/default-image.jpg";
 const HomePage = ({ item }) => {
-  console.log("item id: ", item.id);
-
-  return (
-    <div className="card-container">
-      <Link to={`item/${item.id}`}>
-        <img className="default-image" src={defaultImage} />
-        <div className="item-prev-info">
-          <p>{item.title}</p>
-          <p>1700kr</p>
-        </div>
-      </Link>
-    </div>
-  );
+	return (
+		<div className="card-container">
+			<Link to={`item/${item.id}`}>
+				<img className="default-image" src={defaultImage} />
+				<div className="item-prev-info">
+					<p>{item.title}</p>
+					<p>1700kr</p>
+				</div>
+			</Link>
+		</div>
+	);
 };
 
 export default HomePage;
