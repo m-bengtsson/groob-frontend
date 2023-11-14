e5487a2d75f7a93aaaa4068e2457db11f589a85e
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-11-14 13:25:51 +0100

    Add public view
    Display price only when logged in

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,16 +4,25 @@ import { useContext, useEffect } from "react";
 import { ItemsContext } from "../context/items";
 import ItemCard from "../components/ItemCard";
 import instance from "../axiosconfig";
+import { CurrentUserContext } from "../context/currentUser";
 
 const HomePage = () => {
   const { items, setItems, errorMessage, setErrorMessage } =
     useContext(ItemsContext);
+  const { currentUser } = useContext(CurrentUserContext);
+
+  //console.log("current user: ", currentUser);
 
   useEffect(() => {
     setErrorMessage("");
     if (items.length === 0) {
       const getItems = async () => {
         try {
+          if (!currentUser) {
+            const response = await instance.get("/items/public");
+            const items = response.data;
+            setItems(items);
+          }
           const response = await instance.get("/items");
           const items = response.data;
           setItems(items);
@@ -25,7 +34,7 @@ const HomePage = () => {
       };
       getItems();
     }
-  }, [items, setErrorMessage, setItems]);
+  }, [items, setErrorMessage, setItems, currentUser]);
 
   if (!items) {
     return <div>Loading...</div>;
